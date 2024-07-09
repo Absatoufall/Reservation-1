03e2d97dff6375e4a599af1ba83a8590efecde94
Absatoufall <128919144+Absatoufall@users.noreply.github.com>
2024-07-09 03:49:56 +0000

    Projet_Memoire_modiEnsavoirplus_

--- src/main/java/projet/stage/memoire/controller/EnSavoirPlusController.java
+++ src/main/java/projet/stage/memoire/controller/EnSavoirPlusController.java
@@ -7,8 +7,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EnSavoirPlusController {
 
     @GetMapping("/ensavoirplus")
-    public String football(Model model) {
+    public String ensavoirplusLeopold(Model model) {
         return "ensavoirplusLeopold"; // Retourne le nom du template Thymeleaf sans extension
     }
+
+    @GetMapping("/ensavoirplus1")
+    public String ensavoirplusLatdior(Model model) {
+        return "ensavoirplusLatdior"; // Retourne le nom du template Thymeleaf sans extension
+    }
+
+    @GetMapping("/ensavoirplus2")
+    public String ensavoirplusAlineSitoe(Model model) {
+        return "ensavoirplusAlineSitoe"; // Retourne le nom du template Thymeleaf sans extension
+    }
+    @GetMapping("/ensavoirplus3")
+    public String ensavoirplusCaroline(Model model) {
+        return "ensavoirplusCaroline"; // Retourne le nom du template Thymeleaf sans extension
+    }
+    @GetMapping("/ensavoirplus4")
+    public String ensavoirplusNgalanda(Model model) {
+        return "ensavoirplusNgalanda"; // Retourne le nom du template Thymeleaf sans extension
+    }
+    @GetMapping("/ensavoirplus5")
+    public String ensavoirplusDiambars(Model model) {
+        return "ensavoirplusDiambars"; // Retourne le nom du template Thymeleaf sans extension
+    }
+    @GetMapping("/ensavoirplus6")
+    public String ensavoirplusDakarArena(Model model) {
+        return "ensavoirplusDakarArena"; // Retourne le nom du template Thymeleaf sans extension
+    }
+
+    @GetMapping("/ensavoirplus7")
+    public String ensavoirplusAbdoulaye(Model model) {
+        return "ensavoirplusAbdoulaye"; // Retourne le nom du template Thymeleaf sans extension
+    }
+
+
+
 }
 
